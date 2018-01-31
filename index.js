1ed63e7b362750f57f992c63fcb35b970c4a4ac1
Matt Carter <m@ttcarter.com>
2018-01-31 15:47:31 +1000

    BUGFIX: Cache should be able to store arrays

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ function Cache(options, cb) {
 	* @param {function} [cb] The callback to fire when the value was stored. Called as (err, val)
 	* @returns {Object} This chainable cache module
 	*/
-	cache.set = argy('object|scalar [object|scalar] [date] [function]', function(key, val, expiry, cb) {
+	cache.set = argy('object|scalar [object|array|scalar] [date] [function]', function(key, val, expiry, cb) {
 		if (!cache.activeModule) throw new Error('No cache module loaded. Use cache.init() first');
 
 		if (argy.isType(key, 'object')) {
@@ -139,7 +139,7 @@ function Cache(options, cb) {
 	* @param {function} cb The callback to fire with the retrieved value
 	* @returns {Object} This chainable cache module
 	*/
-	cache.get = argy('scalar [scalar] function', function(key, fallback, cb) {
+	cache.get = argy('scalar [object|array|scalar] function', function(key, fallback, cb) {
 		if (!cache.activeModule) throw new Error('No cache module loaded. Use cache.init() first');
 
 		cache.activeModule.get(key, fallback, cb || _.noop);
